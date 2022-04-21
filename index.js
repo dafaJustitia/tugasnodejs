e915751298d9ad1d624ffca083535e804b7031d9
dafaJustitia <dafajustitia05@gmail.com>
2022-04-21 09:47:11 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//import
+const express = require ('express');
+const cors = require ('cors');
+
+//implementasi
+const app = express();
+app.use(cors());
+
+//endpopint admin
+const admin = require('./routes/admin')
+app.use("/admin",admin)
+
+//customer
+const customer = require('./routes/customer')
+app.use("/customer",customer)
+
+//product
+const product = require('./routes/product');
+app.use("/product", product)
+
+//transaksi
+const transaksi = require('./routes/transaksi');
+app.use("/transaksi", transaksi)
+
+app.listen(8080, () => {
+    console.log ('server run on port 8080')
+})
